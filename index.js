43076be1e1ece81d3491fb747fd095c751e94e0a
Meyvisal <visalmey007@gmail.com>
2026-04-07 11:14:13 +0700

    update message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.write('Hello from CI/CD! 🐳');
+    res.write('Hello from CI/CD! I'm so happy 🐳');
     res.end();
 });
 
