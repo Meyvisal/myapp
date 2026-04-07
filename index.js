5fe931af9991cb5665b40caca2add04679f55988
Meyvisal <visalmey007@gmail.com>
2026-04-07 11:08:16 +0700

    update message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.write('Hello from Docker to CI/CD! 🐳');
+    res.write('Hello from CI/CD! 🐳');
     res.end();
 });
 
